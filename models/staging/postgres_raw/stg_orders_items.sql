79cc3e403462d7ba5fc424b308ad29a45f5e0885
Edith Vidal <ecsr.vidal@gmail.com>
2026-07-23 13:46:16 -0300

    Removendo cast de stg_orders_items, pois já estava como NUMERIC no BigQuery

--- models/staging/postgres_raw/stg_orders_items.sql
+++ models/staging/postgres_raw/stg_orders_items.sql
@@ -7,7 +7,7 @@ select
     order_item_id,
     product_id,
     seller_id,
-    cast(price as decimal(10, 2)) as price,
-    cast(freight_value as decimal(10, 2)) as freight_value,
+    price,
+    freight_value,
     shipping_limit_date
 from {{ source('postgres_raw', 'olist_order_items') }}
